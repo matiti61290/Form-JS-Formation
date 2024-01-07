40a80da9c62cf9c65b4910cce5f5ab3e4a8a8f1f
Mathieu <mathieu.barbey18@gmail.com>
2024-01-07 12:04:24 +0100

    add the form content in the comment list

--- script/script.js
+++ script/script.js
@@ -1,43 +1,57 @@
 //put the form in variables
 let form = document.querySelector("form");
-let firstName = document.getElementById("first-name");
-let lastName = document.getElementById("last-name");
-let comment = document.getElementById("message");
+let newFirstName = document.getElementById("first-name");
+let newLastName = document.getElementById("last-name");
+let newComment = document.getElementById("message");
+let commentList = document.getElementById("comment-list");
 
 //function to check the length of fields
 function validate() {
-    return firstName.value.length && lastName.value.length && comment.value.length;
+    return newFirstName.value.length && newLastName.value.length && newComment.value.length;
 }
 
 function addCommentToList() {
-    //Create the first div
-    let commentList = document.getElementById("comment-list");
-    let commentArea = document.createElement("div");
-    let commentAreaStyle = document.createAttribute("class");
-    commentAreaStyle.value = "flex space-x-4 text-sm text-gray-500";
-    commentArea.setAttributeNode(commentAreaStyle);
+    //Create the new comment area in the list
+    let newCommentList = document.createElement("div");
+    let newCommentListStyle = document.createAttribute("class");
+    newCommentListStyle.value = "flex space-x-4 text-sm text-gray-500";
+    newCommentList.setAttributeNode(newCommentListStyle);
 
-    //create the second div
-    let nameArea = document.createElement("div");
-    let nameAreaStyle = document.createAttribute("class");
-    nameAreaStyle.value = "flex-1 py-10 border-t border-gray-200";
-    nameArea.setAttributeNode(nameAreaStyle);
+    //create the new content area in the comment area
+    let newContentArea = document.createElement("div");
+    let newContentAreaStyle = document.createAttribute("class");
+    newContentAreaStyle.value = "flex-1 py-10 border-t border-gray-200";
+    newContentArea.setAttributeNode(newContentAreaStyle);
 
-    //Add name of the comment
-    let name = document.createElement("h3");
-    let nameStyle = document.createAttribute("class");
-    nameStyle.value = "font-medium text-gray-900";
-    name.setAttributeNode(nameStyle);
-    let nameText = document.createTextNode(firstName.value + " " + lastName.value);
+    //Add first name and last name used in the form
+    let newNameTitle = document.createElement("h3");
+    let newNameTitleStyle = document.createAttribute("class");
+    newNameTitleStyle.value = "font-medium text-gray-900";
+    newNameTitle.setAttributeNode(newNameTitleStyle);
+    let nameText = document.createTextNode(newFirstName.value + " " + newLastName.value);
 
-    //add the new comment
-    name.appendChild(nameText);
-    nameArea.appendChild(name)
-    commentArea.appendChild(nameArea)
-    commentList.appendChild(commentArea);
+    //add the comment area
+    let newCommentarea = document.createElement("div")
+    let newCommentareaStyle = document.createAttribute("class")
+    newCommentareaStyle.value = "prose prose-sm mt-4 max-w-none text-gray-500"
+    newCommentarea.setAttributeNode(newCommentareaStyle)
+
+    //add the comment written in the form
+    let comment = document.createElement("p")
+    let commentText = document.createTextNode(newComment.value)
+
+    //add the name to the HTML
+    newNameTitle.appendChild(nameText);
+    newContentArea.appendChild(newNameTitle)
+    newCommentList.appendChild(newContentArea)
+    commentList.appendChild(newCommentList);
+
+    //add the comment in the HTML
+    comment.appendChild(commentText)
+    newContentArea.appendChild(comment)
 }
 
-//function to display the error message if nothing is written in one field
+//function to add the event when submit the form
 form.addEventListener("submit", function(event) {
     event.preventDefault();
     if(validate() == 0){
@@ -45,4 +59,5 @@ form.addEventListener("submit", function(event) {
         errorMessage.style.display = "block";
         return;
     }
+    addCommentToList()
 })
